3c3a24f8c2353d9ac84fded8ae1ee506afbf0c7c
Michael Boisvert <michaelboisvert6@gmail.com>
2024-11-28 21:17:57 -0700

    Merge fixes

--- src/main/java/org/quantumbadger/redreader/reddit/RedditAPI.java
+++ src/main/java/org/quantumbadger/redreader/reddit/RedditAPI.java
@@ -68,6 +68,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
